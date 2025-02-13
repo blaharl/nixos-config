975d2bb01ab6b738728ae8338c99a92b03af7d5f
pointing <pointing@disroot.org>
2025-02-13 22:06:00 +0900

    init

---
+++ config/packages/dev.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    packages = with pkgs; [
+      # cargo # not compatible with rustup
+      gcc
+      gdb
+      gdbgui # browser based gdb frontend
+      gef
+      git
+      git-filter-repo
+      gittyup
+      gitui
+      gnumake
+      hugo
+      luarocks
+      # ninja 
+      nodejs 
+      postgresql
+      python3Full
+      # python.pkgs.pip
+      python311Packages.pip
+      R
+      rstudio
+      rustup # collision with cargo
+      sqlite
+      # stdenv 
+      texliveFull
+      typst
+      uv
+    ];
+    file = {
+      # ".clangd".source = ./file/common/clangd;
+      # g++ -E -v -xc++ /dev/null
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userName = "user";
+    userEmail = "user@localhost";
+    extraConfig = {
+      core = {
+        sshCommand = "/usr/bin/env ssh -i /home/user/.ssh/id_github";
+      };
+    };
+  };
+}
---
+++ config/packages/temp.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    packages = with pkgs; [
+      audacity
+      bitcoin
+      electrum
+      fontconfig
+      freetype  # font rendering engine
+      # gedit
+      inkscape
+      # libverto 
+      libresprite
+      losslesscut-bin
+      monero-cli
+      monero-gui
+      # sxhkd # simple X hotkey daemon
+      # synergy # share mouse and keyboard between multiple computers
+      ttygif
+      t-rec
+    ];
+  };
+}
