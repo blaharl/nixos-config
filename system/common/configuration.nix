8608f2c167db93b50f02eb3681d727653d35f541
pointing <pointing@disroot.org>
2025-05-12 15:54:47 +0900

    added bluetui

--- system/common/configuration.nix
+++ system/common/configuration.nix
@@ -195,6 +195,7 @@ KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0666", TAG+="uaccess"
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    bluetui
     bluez
     bluez-tools
     nftables
