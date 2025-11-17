1432d29bb3fe417c13a0d42f3d83300e72d16e51
pointing <pointing@disroot.org>
2025-11-18 01:51:14 +0900

    removed nvim magick, texlive

--- config/packages/terminal.nix
+++ config/packages/terminal.nix
@@ -75,8 +75,8 @@ in
 
   programs.neovim = {
       enable = true;
-      extraLuaPackages = ps: [ ps.magick ];
-      extraPackages = [ pkgs.imagemagick ];
+      # extraLuaPackages = ps: [ ps.magick ];
+      # extraPackages = [ pkgs.imagemagick ];
       # ... other config
   };
 
