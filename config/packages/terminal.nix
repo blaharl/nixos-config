3ed5acde0ca8fdae9be5728eb09bef768e2b08cd
pointing <pointing@disroot.org>
2025-12-14 02:45:17 +0900

    updated shell aliases

--- config/packages/terminal.nix
+++ config/packages/terminal.nix
@@ -7,41 +7,27 @@ let
     l="lsd -lA";
     ".." = "cd ..";
 
+    asr="atuin scripts run";
+    asl="atuin scripts list";
+
     mdl="megatools dl --choose-files";
     ydl="yt-dlp";
-    ydls="yt-dlp --write-subs --write-auto-subs --sub-langs \"en.*,ja.*\"";
-    chksum="/home/user/.dotfiles/scripts/chksum.sh";
-    yloc="/home/user/.dotfiles/scripts/yt-local.sh";
     python="python3";
-    shizuku="adb shell /data/app/~~xRLFVsE7GO5RA1EBnl0B1g==/moe.shizuku.privileged.api--lXePlgZQxJGueiHbu5pbw==/lib/arm64/libshizuku.so";
 
     vpnon="mullvad connect; sleep 1; mullvad reconnect";
     vpnoff="mullvad disconnect";
-    vpnloc="mullvad relay set location";
-    vpnlist="mullvad relay list | bat";
-    vpnq="mullvad relay list | grep";
+    vpnloc="mullvad relay set location $(mullvad relay list | fzf | awk '{print $1}')";
     wghome="sudo wg-quick up ~/.dotfiles/private/wg_home.conf";
     wgoff="sudo wg-quick down ~/.dotfiles/private/wg_home.conf";
-
-    umu-run="flatpak run --env=GAMEID= --env=WINEPREFIX=/home/user/Persistent/home/shared/umu/PREFIX/ org.openwinecomponents.umu.umu-launcher";
 
     q="exit";
 
     v="nvim";
     sv="sudo nvim";
 
-    myip="curl ifconfig.me | awk -F \".\" '{print \$1\".\"\$2\".*.*\"}'";
-    myipfull="curl ifconfig.me";
-
-    nixgc="nix-collect-garbage -d; sudo nix-collect-garbage -d";
-    nixgc7d="nix-collect-garbage --delete-older-than 7d; sudo nix-collect-garbage --delete-older-than 7d";
-
     mdv="litemdview";
 
-    clhist="clear && rm ~/.bash_history && history -c";
-
-    ssync="sh /home/user/.dotfiles/private/ssync.sh";
-    myssh="sh /home/user/.dotfiles/private/myssh.sh";
+    clhist="rm ~/.bash_history; clear; history -c";
   };
 in
 
